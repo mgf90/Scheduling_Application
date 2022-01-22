e550ae5086f60feebe1923a7f97b09d97de692f5
Gavin Farmer <mgavinfarmer@gmail.com>
2022-01-22 14:59:07 -0500

    cleaned up JavaFx design

--- src/Model/RunMain.java
+++ src/Model/RunMain.java
@@ -1,10 +1,10 @@
-//package Model;
-//
-//import java.sql.SQLException;
-//
-//public class RunMain {
-//
-//    public static void main(String[] args) throws SQLException {
-//        Main.main(args);
-//    }
-//}
+package Model;
+
+import java.sql.SQLException;
+
+public class RunMain {
+
+    public static void main(String[] args) throws SQLException {
+        Main.main(args);
+    }
+}
